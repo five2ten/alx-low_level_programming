0bcdb8c082a24e7aa8308d7eef86302a83b981cd
matthew adeoyin <adeoyinmatthew@gmail.com>
2022-09-10 21:53:07 -0700

    b

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+/**
+ This is a programme to print
+ Programming is like building a multilingual puzzle
+ return <0>
+*/
+
+int main(void)
+
+{
+	puts("/"Programming is like building a multilingual puzzle");
+	return(0);
+}
+
