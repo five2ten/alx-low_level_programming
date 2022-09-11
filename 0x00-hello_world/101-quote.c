925fca766c417babbfd81b410c0b743525b0a43f
matthew adeoyin <adeoyinmatthew@gmail.com>
2022-09-11 02:56:10 -0700

    mm

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include<unistd.h>
 
 /**
  *
@@ -8,10 +8,9 @@
  *
  *   
 */
-
 int main(void)
 {
-	write(2,"and that piece of art is useful\"- Dora Korpar,
-		       	2015-10-19\n",59);
+	write(2,"and that piece of art is useful\"- Dora Korpa 
+	2015-10-19\",59);
 	return (1);
 }
