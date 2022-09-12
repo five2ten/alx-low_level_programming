2e94f887e7ba9f3ae466a7c2dea93e07ad989458
matthew adeoyin <adeoyinmatthew@gmail.com>
2022-09-12 15:47:33 -0700

    kk

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ else if ((n % 10) < 6 && (n % 10) != 0)
 }
 else
 {
-	printf("Last digit of %d is %d and is 0\n",n, n % 10);
+	printf("Last digit of %d is %d and is 0\n", n, n % 10);
 }
 return (0);
 }
