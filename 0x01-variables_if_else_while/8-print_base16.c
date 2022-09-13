7566a3e415c8d47f659ad3b326387692459d4255
matthew adeoyin <adeoyinmatthew@gmail.com>
2022-09-12 18:04:45 -0700

    f

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * *main - print if the number is postive, zero, or negative
+ *
+ *Description: using the main function
+ *
+ *this program prints Programming is positive, zero, or neg
+ *Return: 0
+ *
+ * */
+int main(void)
+{
+char d;
+for (d = '0'; d <= '9'; d++)
+{
+	putchar(d);
+}
+for (d = 'a'; d = 'f'; d++)
+{
+	putchar(d);
+}
+putchar9('\n');
+return (0);
+}
