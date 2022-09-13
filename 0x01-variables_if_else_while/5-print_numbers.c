cf0779426646d3ef50559465db70c44c9bc779b7
matthew adeoyin <adeoyinmatthew@gmail.com>
2022-09-12 18:30:16 -0700

    m

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
  *main - Write a program that prints all single digit numbers of 1o in
- *Description the main function
+ *Description : the main function
  *this program prints "Programming is positive, zero, or negative"
  *
  * Return 0
